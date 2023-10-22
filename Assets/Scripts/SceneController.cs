159ea6e86dc1df63d8aeaf4eb18ae1440de5343a
Mem222222 <matthewb40000@gmail.com>
2023-10-22 13:40:17 -0400

    Added som more changes to the menus

--- Assets/Scripts/SceneController.cs
+++ Assets/Scripts/SceneController.cs
@@ -10,6 +10,16 @@ public class SceneController : MonoBehaviour
     {
         SceneManager.LoadScene(DisScene);
     }
-
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape)) //Closes the game mid-gameplay
+        {
+           GetComponent<CloseProgram>().CloseGame();
+        }
+        if (Input.GetKeyDown(KeyCode.R)) //Test key for death screen
+        {
+            SceneManager.LoadScene(2);
+        }
+    }
 
 }
